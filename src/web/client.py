afd29c4cdc3b8472d644274d965cb36301d6e6e2
kl <klpig213@gmail.com>
2024-12-30 15:15:21 +0800

    Add plantera

--- src/web/client.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-
-class Client:
-    def __init__(self, addr, port):
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((addr, port))
